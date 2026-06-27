0f16a415dc405d28b5cc63a3d8248b4e1e2239b7
codegraphtheory <codegraphtheory@pm.me>
2026-06-27 18:35:53 -0400

    feat(demos): Hermes skinned TUI VHS recordings

--- demos/vhs/sanitize-recording-env.sh
+++ demos/vhs/sanitize-recording-env.sh
@@ -9,8 +9,13 @@ export HOME="$_REPO_ROOT/demos/vhs/staging/home/graphtheory"
 export USER=graphtheory
 export LOGNAME=graphtheory
 export HOSTNAME=cyber
+export HERMES_HOME="$HOME/.hermes"
+export HERMES_TUI_FAST_ECHO=0
+unset HERMES_TUI_THEME
+unset VSCODE_IPC_HOOK_CLI
+unset TERM_PROGRAM
 
-mkdir -p "$HOME/users/graphtheory/projects"
+mkdir -p "$HERMES_HOME" "$HOME/users/graphtheory/projects"
 _WORKSPACE="$HOME/users/graphtheory/projects/$(basename "$_REPO_ROOT")"
 ln -sfn "$_REPO_ROOT" "$_WORKSPACE"
 
